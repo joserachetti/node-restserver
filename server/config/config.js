ba42bdceda925596f99e8dface1502023a31dd68
Jose Rachetti <joseballe9@gmail.com>
2020-02-13 04:17:02 -0300

    RESTServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,23 @@
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =========================
+//         ENTORNO
+// =========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================
+//         ENTORNO
+// =========================
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://matrix:bobv0v1IoHsTPUbd@cluster0-dlhgs.mongodb.net/cafe?retryWrites=true&w=majority';
+// }
+
+process.env.URLDB = urlDB;
